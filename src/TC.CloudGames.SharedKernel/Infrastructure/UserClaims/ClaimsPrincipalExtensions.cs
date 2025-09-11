28a4d0a69f8110de6456ed24515eb0b04bacdc42
rdpresser <rodrigo.presser@gmail.com>
2025-09-11 02:27:28 -0300

    Enhance FastEndpoints and ClaimsPrincipal handling
    - Added custom settings for FastEndpoints in ApplicationBuilderExtensions.cs, including role claim type, route prefix, and error response metadata.
    - Updated AddCustomAuthentication in ServiceCollectionExtensions.cs to use JWT settings from configuration for Bearer authentication.
    - Improved user claim retrieval in ClaimsPrincipalExtensions.cs by adding fallback mechanisms for user ID, email, name, username, and role.

--- src/TC.CloudGames.SharedKernel/Infrastructure/UserClaims/ClaimsPrincipalExtensions.cs
+++ src/TC.CloudGames.SharedKernel/Infrastructure/UserClaims/ClaimsPrincipalExtensions.cs
@@ -4,42 +4,58 @@
     {
         public static Guid GetUserId(this ClaimsPrincipal? principal)
         {
-            string? userId = principal?.FindFirstValue(JwtRegisteredClaimNames.Sub);
-            return Guid.TryParse(userId, out Guid parsedUserId) ?
-                parsedUserId :
-                throw new InvalidOperationException("User id is unavailable");
+            string? userId =
+                principal?.FindFirstValue(JwtRegisteredClaimNames.Sub) ??
+                principal?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return Guid.TryParse(userId, out Guid parsedUserId)
+                ? parsedUserId
+                : throw new InvalidOperationException("User id is unavailable");
         }
 
         public static string GetUserEmail(this ClaimsPrincipal? principal)
         {
-            string? userEmail = principal?.FindFirstValue(JwtRegisteredClaimNames.Email);
-            return string.IsNullOrEmpty(userEmail) ?
-                throw new InvalidOperationException("User email is unavailable") :
-                userEmail;
+            string? userEmail =
+                principal?.FindFirstValue(JwtRegisteredClaimNames.Email) ??
+                principal?.FindFirstValue(ClaimTypes.Email);
+
+            return string.IsNullOrEmpty(userEmail)
+                ? throw new InvalidOperationException("User email is unavailable")
+                : userEmail;
         }
 
         public static string GetUserName(this ClaimsPrincipal? principal)
         {
-            string? userName = principal?.FindFirstValue(JwtRegisteredClaimNames.Name);
-            return string.IsNullOrEmpty(userName) ?
-                throw new InvalidOperationException("User name is unavailable") :
-                userName;
+            string? userName =
+                principal?.FindFirstValue(JwtRegisteredClaimNames.Name) ??
+                principal?.FindFirstValue(ClaimTypes.Name);
+
+            return string.IsNullOrEmpty(userName)
+                ? throw new InvalidOperationException("User name is unavailable")
+                : userName;
         }
 
         public static string GetUserUsername(this ClaimsPrincipal? principal)
         {
-            string? userName = principal?.FindFirstValue(JwtRegisteredClaimNames.UniqueName);
-            return string.IsNullOrEmpty(userName) ?
-                throw new InvalidOperationException("User Username is unavailable") :
-                userName;
+            string? userName =
+                principal?.FindFirstValue(JwtRegisteredClaimNames.UniqueName) ??
+                principal?.FindFirstValue(ClaimTypes.Name);
+
+            return string.IsNullOrEmpty(userName)
+                ? throw new InvalidOperationException("User Username is unavailable")
+                : userName;
         }
 
         public static string GetUserRole(this ClaimsPrincipal? principal)
         {
-            string? userRole = principal?.FindFirstValue("role");
-            return string.IsNullOrEmpty(userRole) ?
-                throw new InvalidOperationException("User role is unavailable") :
-                userRole;
+            string? userRole =
+                principal?.FindFirstValue("role") ??
+                principal?.FindFirstValue(ClaimTypes.Role);
+
+            return string.IsNullOrEmpty(userRole)
+                ? throw new InvalidOperationException("User role is unavailable")
+                : userRole;
         }
     }
+
 }
