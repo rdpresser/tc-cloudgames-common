0dbade0a6ac200ebb868d971a50ce2c7b067b9fd
rdpresser <rodrigo.presser@gmail.com>
2025-09-11 16:51:39 -0300

    Refactor game-related properties and improve imports
    - Added and removed global using directives in AssemblyInfo.cs for better organization.
    - Renamed property `Platform` to `Platforms` for consistency across multiple files.
    - Updated `ServiceCollectionExtensions.cs` with new schema configurations for `GameProjection`.
    - Refactored `GameProjection` and `GameProjectionHandler` to support async methods and new property types.
    - Cleaned up unused using directives and removed obsolete methods to enhance code clarity.
    - Adjusted validation methods in `GameDetails` and `GameAggregate` to align with the new structure.

--- src/TC.CloudGames.Contracts/Events/Games/GameIntegrationEvent.cs
+++ src/TC.CloudGames.Contracts/Events/Games/GameIntegrationEvent.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TC.CloudGames.Contracts.Events.Games
+﻿namespace TC.CloudGames.Contracts.Events.Games
 {
     // ------------------------------------------------------------------------------------
     // Integration Events
